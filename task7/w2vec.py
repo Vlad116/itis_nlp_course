7c84fbda27b995274752248e2891b3692532c78c
Vladislav <vladalekseev9@yandex.ru>
2020-04-15 12:43:29 +0300

    :zap: add Task6

--- task7/w2vec.py
+++ task7/w2vec.py
@@ -1,30 +1,19 @@
-import keras
-import numpy as np
-from keras import backend as ker
-from collections import Counter
 import math
+import string
+from collections import Counter
 from itertools import islice
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
 import nltk
+import numpy as np
 import pandas
 import pymorphy2
-
-
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.metrics import precision_score
-# from sklearn.metrics import recall_score
-# from sklearn.metrics import f1_score
-# from sklearn.metrics import accuracy_score
-# Using TensorFlow backend.
+from keras import backend as ker
 
 def get_text_in_normal_form(text):
     analyzer = pymorphy2.MorphAnalyzer()
-    punctuation = [',', '.', ':', '?', '«', '»', '-', '(', ')', '!', '\'', '—', ';', '”', '...', '#', '$', '%', '&']
     tokens = nltk.word_tokenize(text)
     normalized_words = []
     for token in tokens:
-        if token in punctuation: continue
+        if token in string.punctuation: continue
         normalized_words.append(analyzer.parse(token)[0].normal_form)
     return normalized_words
 
@@ -106,7 +95,6 @@ def f1_m(y_true, y_pred):
     recall = recall_m(y_true, y_pred)
     return 2 * ((precision * recall) / (precision + recall + ker.epsilon()))
 
-
 # Cчитали все отзывы
 filename = 'reviews.csv'
 df = pandas.read_csv(filename, encoding="utf-8")
@@ -122,53 +110,11 @@ normalized_texts_predict = get_notmalized_texts(test_data)
 print('--- Normalization end ---')
 
 print('--- Computing TF-IDF begin ---')
-tf_idf_train = compute_tf_idf(normalized_texts_train)
-tf_idf_predict = compute_tf_idf(normalized_texts_predict)
+tfidf_train = compute_tf_idf(normalized_texts_train)
+tfidf_predict = compute_tf_idf(normalized_texts_predict)
 print('--- Computing TF-IDF end ---')
 
 # Выделяем 500 самых частых слов
 frequency = compute_frequency(normalized_texts_train)
 frequency = {k: v for k, v in sorted(frequency.items(), key=lambda item: item[1], reverse=True)}
 frequency = dict(islice(frequency.items(), 0, 500))
-
-print("--- Getting vectors ---")
-# Получаем вектора для тренировки нейронной сети и для предсказания
-x_train_vector = get_tf_idf_vector(tf_idf_train, frequency)
-y_train_vector = get_tf_idf_vector(tf_idf_predict, frequency)
-print("--- End getting vectors ---")
-
-num_classes = 3
-# y_train = keras.utils.to_categorical(y_train, num_classes)
-# y_test = keras.utils.to_categorical(y_test, num_classes)
-
-# Настройка слоев нейронной сети
-model = Sequential()
-# Input - Layer
-model.add(Dense(512, input_shape=(500,)))
-model.add(Activation('relu'))
-# Cкрытый полносвязный
-model.add(Dense(512))
-# Dropout слой
-model.add(Dropout(0.5))
-# model.add(Dense(num_classes))
-# model.add(Activation('softmax'))
-# model.add(Dense(512))
-model.add(Dense(3, activation='softmax'))
-# model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', f1_m, precision_m, recall_m])
-
-print("---TRAINING---")
-batch_size = 32
-epochs = 10
-model.fit(np.array(x_train_vector), np.array(df[['label']]), epochs=epochs, batch_size=batch_size)
-# np.array(df[['label']])
-
-print("---PREDICT---")
-loss, accuracy, f1_score, precision, recall = model.evaluate(y_train_vector, test_data['label'], verbose=0)
-
-print("Test loss:", loss)
-print("Test accuracy:", accuracy)
-print("F1:", f1_score)
-print("Precision:", precision)
-print("Recall:", recall)
