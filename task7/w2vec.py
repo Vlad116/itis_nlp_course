6e58e8e16e9f8d75b06366723fd4603d364ac07d
Vladislav <vladalekseev9@yandex.ru>
2020-04-22 08:39:25 +0300

    :art: add w2vec task

--- task7/w2vec.py
+++ task7/w2vec.py
@@ -1,120 +1,90 @@
-import math
-import string
-from collections import Counter
-from itertools import islice
-import nltk
-import numpy as np
+import nltk as nltk
 import pandas
 import pymorphy2
-from keras import backend as ker
+import numpy as np
+import gensim.models
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_recall_fscore_support
+
+
+def make_feature_vector(words, model, num_features):
+    feature_vector = np.zeros((num_features,), dtype="float32")
+    words_count = 0
+    index2word_set = set(model.wv.index2word)
+    for word in words:
+        if word in index2word_set:
+            words_count = words_count + 1
+            feature_vector = np.add(feature_vector, model[word])
+
+    feature_vector = np.divide(feature_vector, words_count)
+    return feature_vector
+
+
+def get_avg_feature_vectors(reviews, model, num_features):
+    counter = 0
+    review_feature_vectors = np.zeros((len(reviews), num_features), dtype='float32')
+    for review in reviews:
+        review_feature_vectors[counter] = make_feature_vector(review, model, num_features)
+        counter = counter + 1
+    return review_feature_vectors
+
 
 def get_text_in_normal_form(text):
     analyzer = pymorphy2.MorphAnalyzer()
+    normalized_text = []
     tokens = nltk.word_tokenize(text)
-    normalized_words = []
     for token in tokens:
-        if token in string.punctuation: continue
-        normalized_words.append(analyzer.parse(token)[0].normal_form)
-    return normalized_words
-
-
-def get_notmalized_texts(data):
-    normalized_texts = []
-    i = 0
-    for elem in data.values:
-        print("Iteration: {}".format(i))
-        normalized_texts.append(get_text_in_normal_form(elem[1]))
-        i = i + 1
-    return normalized_texts
-
-
-def compute_tf_idf(corpus):
-    def compute_tf(text):
-        tf_text = Counter(text)
-        for i in tf_text:
-            tf_text[i] = tf_text[i] / float(len(text))
-        return tf_text
-
-    def compute_idf(word, corpus):
-        return math.log10(len(corpus) / sum([1.0 for i in corpus if word in i]))
-
-    documents_list = []
-    for text in corpus:
-        tf_idf_dictionary = {}
-        computed_tf = compute_tf(text)
-        for word in computed_tf:
-            tf_idf_dictionary[word] = computed_tf[word] * compute_idf(word, corpus)
-        documents_list.append(tf_idf_dictionary)
-    return documents_list
-
-
-def compute_frequency(all_text):
-    frequency = {}
-    for text in all_text:
-        for word in text:
-            count = frequency.get(word, 0)
-            frequency[word] = count + 1
-    return frequency
-
-
-def get_tf_idf_vector(tfidf_list, frequency):
-    vectors = []
-    j = 0
-    for text in tfidf_list:
-        tfidf_vector = np.zeros(len(frequency))
-        for w in list(text):
-            for i, word in enumerate(list(frequency)):
-                if word == w:
-                    tfidf_vector[i] = text.get(w, 0)
-        vectors.append(tfidf_vector)
-        j += 1
-        print("iteration: " + str(j))
-    return vectors
+        normalized_text.append(analyzer.parse(token)[0].normal_form)
+    return normalized_text
 
 
 def filter_by_reviews_title(data_frame, reviews_titles):
     return data_frame[~data_frame['title'].isin(reviews_titles)]
 
 
-def recall_m(y_true, y_pred):
-    true_positives = ker.sum(ker.round(ker.clip(y_true * y_pred, 0, 1)))
-    possible_positives = ker.sum(ker.round(ker.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + ker.epsilon())
-    return recall
-
-
-def precision_m(y_true, y_pred):
-    true_positives = ker.sum(ker.round(ker.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = ker.sum(ker.round(ker.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + ker.epsilon())
-    return precision
-
-
-def f1_m(y_true, y_pred):
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
-    return 2 * ((precision * recall) / (precision + recall + ker.epsilon()))
-
-# Cчитали все отзывы
-filename = 'reviews.csv'
-df = pandas.read_csv(filename, encoding="utf-8")
+# Считываем все отзывы
+df = pandas.read_csv("reviews.csv", encoding="utf-8")
+df['text'] = df['text'].map(lambda x: get_text_in_normal_form(x))
 my_reviews = ['Матрица ', '1+1', 'Хоббит: Нежданное путешествие']
 
-# Отделили тестовую выборку
+# Отделяем тестовые отзывы
 test_data = df[df['title'].isin(my_reviews)]
 df = filter_by_reviews_title(df, my_reviews)
 
-print('--- Normalization data begin ---')
-normalized_texts_train = get_notmalized_texts(df)
-normalized_texts_predict = get_notmalized_texts(test_data)
-print('--- Normalization end ---')
+# Создаем модель word2vec
+model = gensim.models.Word2Vec(sentences=df['text'], min_count=3, iter=300)
 
-print('--- Computing TF-IDF begin ---')
-tfidf_train = compute_tf_idf(normalized_texts_train)
-tfidf_predict = compute_tf_idf(normalized_texts_predict)
-print('--- Computing TF-IDF end ---')
+# Синонимы
+print(model.wv.most_similar(positive=['фильм'], topn=5))
+print(model.wv.most_similar(positive=['плохой'], topn=5))
+print(model.wv.most_similar(positive=['работа'], topn=5))
+print(model.wv.most_similar(positive=['оценка'], topn=5))
+print(model.wv.most_similar(positive=['шок'], topn=5))
 
-# Выделяем 500 самых частых слов
-frequency = compute_frequency(normalized_texts_train)
-frequency = {k: v for k, v in sorted(frequency.items(), key=lambda item: item[1], reverse=True)}
-frequency = dict(islice(frequency.items(), 0, 500))
+# Получаем вектора
+trainDataVectors = get_avg_feature_vectors(df['text'], model, 100)
+testDataVectors = get_avg_feature_vectors(test_data['text'], model, 100)
+
+# Тренируем модель (лучший результат был у RF, поэтому выбираем его)
+rfc = RandomForestClassifier(n_estimators=100, max_depth=20, random_state=0)
+rfc.fit(trainDataVectors, df['label'])
+
+# Применяем модель к текстовым текстам
+predicted = rfc.predict(testDataVectors)
+
+# Вычисляем Accuracy
+true_positives = 0
+k = 0
+
+for prediction in predicted:
+    if prediction == test_data.values[k][2]:
+        true_positives += 1
+    k += 1
+
+print("Accuracy: {}".format(true_positives / len(predicted)))
+
+# Вычисляем Precision, Recall, Fscore
+precision_recall_fscore = precision_recall_fscore_support(test_data['label'].values, predicted)
+print(f"Precision(-1, 0, 1) = {precision_recall_fscore[0]}")
+print(f"Recall(-1, 0, 1) = {precision_recall_fscore[1]}")
+print(f"Fscore(-1, 0, 1) = {precision_recall_fscore[2]}")
