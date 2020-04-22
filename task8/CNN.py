b8f9a61ef5109ccd125d6e2b6157a42f558a6c0c
Vladislav <vladalekseev9@yandex.ru>
2020-04-22 15:46:07 +0300

    :art: task 8 fix problems with words

--- task8/CNN.py
+++ task8/CNN.py
@@ -5,25 +5,28 @@ from keras.layers import Dense, Dropout, Activation, Input, Embedding, Conv1D, G
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras import backend as ker
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import precision_score
+# from sklearn.metrics import recall_score
+# from sklearn.metrics import f1_score
+# from sklearn.metrics import accuracy_score
 from keras.objectives import categorical_crossentropy
 import pandas
 
 max_words = 500
 batch_size = 32
-epochs = 10
+epochs = 50
 max_len = 40
+
 
 def create_embedding_matrix(filepath, word_index, embedding_dim):
     vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
     embedding_matrix = np.zeros((vocab_size, embedding_dim))
 
-    with open(filepath) as f:
+    with open(filepath, "r", encoding='utf8') as f:
         for line in f:
             word, *vector = line.split()
+            word = word[:word.find('_')]
+            print(word)
             if word in word_index:
                 idx = word_index[word]
                 embedding_matrix[idx] = np.array(
@@ -31,22 +34,26 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
 
     return embedding_matrix
 
-# def recall_m(y_true, y_pred):
-#     true_positives = ker.sum(ker.round(ker.clip(y_true * y_pred, 0, 1)))
-#     possible_positives = ker.sum(ker.round(ker.clip(y_true, 0, 1)))
-#     recall = true_positives / (possible_positives + ker.epsilon())
-#     return recall
-#
-# def precision_m(y_true, y_pred):
-#     true_positives = ker.sum(ker.round(ker.clip(y_true * y_pred, 0, 1)))
-#     predicted_positives = ker.sum(ker.round(ker.clip(y_pred, 0, 1)))
-#     precision = true_positives / (predicted_positives + ker.epsilon())
-#     return precision
-#
-# def f1_m(y_true, y_pred):
-#     precision = precision_m(y_true, y_pred)
-#     recall = recall_m(y_true, y_pred)
-#     return 2 * ((precision * recall) / (precision + recall + ker.epsilon()))
+
+def recall_m(y_true, y_pred):
+    true_positives = ker.sum(ker.round(ker.clip(y_true * y_pred, 0, 1)))
+    possible_positives = ker.sum(ker.round(ker.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + ker.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    true_positives = ker.sum(ker.round(ker.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = ker.sum(ker.round(ker.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + ker.epsilon())
+    return precision
+
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + ker.epsilon()))
+
 
 df_train = pandas.read_csv("reviews.csv")
 df_test = pandas.read_csv("my_reviews.csv")
@@ -103,9 +110,10 @@ print(model.to_yaml())
 # SVG(model_to_dot(model, show_shapes=True).create(prog='dot', format='svg'))
 
 from keras.objectives import categorical_crossentropy
+
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
-              metrics=['accuracy',f1_score, precision_score, recall_score])
+              metrics=['accuracy', f1_m, precision_m, recall_m])
 
 # from keras.callbacks import TensorBoard
 # tensorboard=TensorBoard(log_dir='./logs', write_graph=True, write_images=True)
@@ -147,23 +155,22 @@ vocab_size = len(tokenizer.word_index) + 1
 
 embedding_dim = 300
 embedding_matrix = create_embedding_matrix(
-     '../../model.txt',
+    '../../model.txt',
     tokenizer.word_index, embedding_dim)
 
 model = Sequential()
 model.add(Embedding(input_dim=vocab_size, weights=[embedding_matrix],
                     output_dim=embedding_dim, input_length=max_len,
-                   trainable=True))
+                    trainable=True))
 model.add(Conv1D(128, 3))
 model.add(Activation("relu"))
 model.add(GlobalMaxPool1D())
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
-
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
-              metrics=['accuracy', f1_score, precision_score, recall_score])
+              metrics=['accuracy', f1_m, precision_m, recall_m])
 
 # from keras.callbacks import TensorBoard
 # tensorboard=TensorBoard(log_dir='./logs', write_graph=True, write_images=True)
@@ -180,8 +187,8 @@ history = model.fit(x_train, y_train,
 
 loss, accuracy, f1_score, precision, recall = model.evaluate(x_val, y_val, batch_size=batch_size, verbose=1)
 print('\n')
-print("Test loss:", loss)
-print("Test accuracy:", accuracy)
+print("loss:", loss)
+print("accuracy:", accuracy)
 print("F1:", f1_score)
 print("Precision:", precision)
 print("Recall:", recall)
